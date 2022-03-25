171c0e96c6811a51bbc2d8f3cacd51872d613447
freaklucas <lucas.oliveira.freak@gmail.com>
2022-03-25 20:42:50 -0300

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,62 @@
 <template>
   <v-app :style="{ background: $vuetify.theme.themes.dark.background }">
     <Navbar />
+    <v-container fluid>
+      <v-row>
+        <v-col cols="6">
+          <v-img src="../assets/home.png" container max-height="500"></v-img>
+        </v-col>
+        <v-col cols="6">
+          <h4 class="red--text text-darken-4 top">Hello, Im Lucas</h4>
+          <h1 class="white--text">Developer web</h1>
+          <p class="grey--text">
+            Front end developer focused on user experience and modern interface
+            building.
+            <br />
+          </p>
+          <v-btn tile color="#A7121D" dark>about me</v-btn>
+        </v-col>
+        <v-col cols="12" class="padd">
+          <div class="first" id="project">
+            <v-row>
+              <v-col cols="12">
+                <div class="child bgColor1">
+                  <v-icon color="#A7121D" x-large class="ml-3">
+                    mdi-palette-swatch
+                  </v-icon>
+                  <h3 class="white--text ml-3 mt-4">Front-end</h3>
+                  <p class="grey--text ml-3 mt-6">Modern applications</p>
+                  <v-btn color="#A7121D" dark text>
+                    Know more
+                    <v-icon right>mdi-arrow-right</v-icon>
+                  </v-btn>
+                </div>
+                <div class="child bgColor2">
+                  <v-icon x-large class="ml-3" dark> mdi-shopping </v-icon>
+                  <h3 class="white--text ml-3 mt-4">Design</h3>
+                  <p class="grey--text ml-3 mt-6">Expert UI/UX</p>
+                  <v-btn dark text>
+                    Know more
+                    <v-icon right>mdi-arrow-right</v-icon>
+                  </v-btn>
+                </div>
+                <div class="child bgColor1">
+                  <v-icon color="#A7121D" x-large class="ml-3"
+                    >mdi-book-open-page-variant</v-icon
+                  >
+                  <h3 class="white--text ml-3 mt-4">Branding</h3>
+                  <p class="grey--text ml-3 mt-6">aaaa</p>
+                  <v-btn dark text>
+                    Know more
+                    <v-icon right>mdi-arrow-right</v-icon>
+                  </v-btn>
+                </div>
+              </v-col>
+            </v-row>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
@@ -13,5 +69,103 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.top {
+  margin-top: 180px;
+}
+.topInverse {
+  margin-top: -250px;
+}
+
+.topTolbar {
+  margin-top: 100px;
+  text-align: center;
+}
+
+.first {
+  width: 100%;
+  height: 610px;
+  background: linear-gradient(
+    to right,
+    #181818,
+    #181819 50%,
+    #111111 50%,
+    #111111 50%
+  );
+  text-align: center;
+  padding: 2rem 2rem;
+}
+
+.second {
+  width: 100%;
+  height: 400px;
+  background: #181818;
+  text-align: center;
+
+  padding: 2rem 2rem;
+}
+
+.secondchild1 {
+  display: inline-block;
+  background-color: #1e1e1e;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  text-align: left;
+  margin-top: 250px;
+}
+
+.child {
+  display: inline-block;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  text-decoration: left;
+  margin-right: 8px;
+}
+
+.bgColor1 {
+  background-color: #1e1e1e;
+}
+
+.bgColor2 {
+  background-color: #ce121a;
+}
+
+.child1 {
+  display: inline-block;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  margin-right: 5px;
+  width: 240px;
+}
+
+.child2 {
+  display: inline-block;
+  width: 245px;
+  vertical-align: middle;
+}
+
+.mRight {
+  margin-right: 8px;
+}
+
+.mButton {
+  margin-bottom: 8px;
+}
+
+.padding {
+  padding: 8px 0;
+}
+
+.col-12.padd {
+  padding: 12px 0 !important;
+}
+
+.col-12-childcol {
+  padding: 0 !important;
+}
+
+h1.number {
+  font-size: 50px;
+  font-weight: bold;
+}
 </style>

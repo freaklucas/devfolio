b7ab179d309ca5278a595df82f65cacb3a8699e5
freaklucas <lucas.oliveira.freak@gmail.com>
2022-03-24 23:57:14 -0300

    config ambiente

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar app color="#11111" dark flat class="px-8">
+    <v-btn icon>
+      <v-icon color="#A7121D">fas fa-dragon</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>

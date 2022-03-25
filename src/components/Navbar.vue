b7006dff3834d4d60ce4132b24629988a8c3659c
freaklucas <lucas.oliveira.freak@gmail.com>
2022-03-25 00:38:36 -0300

    navbar ok

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,23 @@
 <template>
   <v-app-bar app color="#11111" dark flat class="px-8">
     <v-btn icon>
-      <v-icon color="#A7121D">fas fa-dragon</v-icon>
+      <v-icon color="#A7121D">fas fa-code</v-icon>
     </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn text @click="scroll('home')">Home</v-btn>
+    <v-btn text @click="scroll('project')">Project</v-btn>
+    <v-btn text @click="scroll('about')">About</v-btn>
+    <v-btn text @click="scroll('contact')">Contact</v-btn>
   </v-app-bar>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    scroll(refName) {
+      const element = this.getElementById(refName);
+      element.scrollIntroView({ behaviour: "smooth" });
+    },
+  },
+};
 </script>

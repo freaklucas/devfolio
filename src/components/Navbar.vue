0a77140a3dfab2c2a7c20ebd6f544090cc5e10c6
freaklucas <lucas.oliveira.freak@gmail.com>
2022-03-28 22:19:01 -0300

    scrool ok

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,8 +5,8 @@
     </v-btn>
     <v-spacer></v-spacer>
     <v-btn text @click="scroll('project')">Project</v-btn>
-    <v-btn text @click="scroll('about')">About</v-btn>
-    <v-btn text @click="scroll('contact')">Contact</v-btn>
+    <v-btn text @click="scroll('about')">about</v-btn>
+    <v-btn text @click="scroll('contact')"> contact</v-btn>
   </v-app-bar>
 </template>
 
@@ -15,7 +15,7 @@ export default {
   methods: {
     scroll(refName) {
       const element = document.getElementById(refName);
-      element.scrollIntroView({ behaviour: "smooth" });
+      element.scrollIntoView({ behavior: "smooth" });
     },
   },
 };
